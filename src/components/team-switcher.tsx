c3dfe3ee1fcfdc8625a5ec3769c6740622bc0d8c
John Lennon <joaooli17@hotmail.com>
2025-02-20 18:23:27 -0300

    pointer no botão de mudar tema

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-
 import { SidebarMenu, SidebarMenuButton } from "@/components/ui/sidebar";
 
 export function ThemeSwitcher() {
+  // Estado para controlar o modo escuro
   const [isDarkMode, setIsDarkMode] = React.useState(true);
 
   // Função para alternar o tema
@@ -16,6 +16,15 @@ export function ThemeSwitcher() {
     }
   };
 
+  // Definir o tema padrão como dark ao montar o componente
+  React.useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
     <SidebarMenu>
       <SidebarMenuButton
@@ -23,7 +32,11 @@ export function ThemeSwitcher() {
         className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground hover:bg-transparent cursor-default data-[active=true]:bg-transparent w-fit aspect-square flex justify-center"
         onClick={toggleTheme}
       >
-        {isDarkMode ? <Moon className="" /> : <Sun className="" />}
+        {isDarkMode ? (
+          <Moon className="cursor-pointer" />
+        ) : (
+          <Sun className="cursor-pointer" />
+        )}
       </SidebarMenuButton>
     </SidebarMenu>
   );
