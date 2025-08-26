94d29d801511c8998f97ac4a2c6a1a293495647b
JoaoOliveira-Dev <joaooli17@hotmail.com>
2025-08-25 22:09:29 -0300

    novo click jacking

--- src/pages/clickJacking.tsx
+++ src/pages/clickJacking.tsx
@@ -5,6 +5,7 @@ export default function ClickJackingPage() {
   const [jacking1, setJacking1] = useState("");
   const [jacking2, setJacking2] = useState("");
   const [jacking3, setJacking3] = useState("");
+  const [jacking4, setJacking4] = useState("");
 
   useEffect(() => {
     fetch("/clickjacking/click_jacking1.txt")
@@ -18,6 +19,10 @@ export default function ClickJackingPage() {
     fetch("/clickjacking/click_jacking3.txt")
       .then((res) => res.text())
       .then((text) => setJacking3(text));
+
+    fetch("/clickjacking/click_jacking_fullscreen.txt")
+      .then((res) => res.text())
+      .then((text) => setJacking4(text));
   }, []);
 
   return (
@@ -26,6 +31,7 @@ export default function ClickJackingPage() {
         <PayloadCard title="Click_Jacking_1()" payload={jacking1} />
         <PayloadCard title="Click_Jacking_2()" payload={jacking2} />
         <PayloadCard title="Click_Jacking_3()" payload={jacking3} />
+        <PayloadCard title="Click_Jacking_FullScreen()" payload={jacking4} />
       </div>
     </div>
   );
